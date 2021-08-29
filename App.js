ee82bb24944c268548543a4b8676f47b53782b76
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-08-29 15:34:54 -0700

    added button components

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   return (
-    <ViewImageScreen/>
-    
+    <View 
+      style={{ 
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log('Tapped')} />
+    </View>
     )
 }
 
