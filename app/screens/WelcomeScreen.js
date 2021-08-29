3dd813f4840ed5d6f9130e3973514b53e3d6cffe
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-08-29 16:35:32 -0700

    welcome screen improved

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
+
+import AppButton from '../components/AppButton';
+import colors from '../config/colors';
+
 function WelcomeScreen(props) {
     return (
         <ImageBackground
+            blurRadius={4}
             style={styles.background}
             source={require('../assets/background.jpg')}>
             <View style={styles.logoContainer}>
                 <Image 
                     style={styles.logo}
                     source={require('../assets/logo-red.png')} />
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagLine}>Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonContainer}>
+                <AppButton title="Login" onPress={() => console.log('closed')} />
+                <AppButton title="Register" color="secondary"  />
+            </View>
         </ImageBackground>
     );
 }
@@ -24,15 +31,9 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
     },
-    loginButton: {
+    buttonContainer: {
         width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",    
-    },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
+        padding: 20,
     },
     logo: {
         width: 100,
@@ -43,9 +44,12 @@ const styles = StyleSheet.create({
         position: "absolute",
         top: 70,
         alignItems: "center",
-    
-    }
-
+    },
+    tagLine: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
+    },
 });
 
 export default WelcomeScreen;
