395443283a94a1e668c08875867adf59688c38ae
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-08-29 20:27:34 -0700

    working on cards

--- App.js
+++ App.js
@@ -5,10 +5,22 @@ import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppButton from "./app/components/AppButton";
+import AppText from "./app/components/AppText";
+import Card from "./app/components/Card";
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+      <View
+        style={{backgroundColor: "#f8f4f4",
+                padding: 20,
+                paddingTop: 10,}}
+      >
+        <Card 
+          title="Red jacket for sale"
+          subTitle="$200"
+          image={require("./app/assets/jacket.jpg")}
+        />
+      </View>
     )
 }
 
---
+++ app/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+
+import colors from '../config/colors';
+import AppText from './AppText';
+
+function Card({ image, title, subTitle }) {
+    return (
+       <View style={styles.card}>
+        <Image style={styles.image} source={image} />
+        <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText>{subTitle}</AppText>
+        </View>
+       </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+      borderRadius: 15,
+      backgroundColor: colors.white,
+      marginBottom: 20
+    },
+    detailsContainer: {
+        padding: 20,
+
+    },
+    image: {
+        width: "100%",
+        height: 200,
+    },
+    title: {
+        marginBottom: 7,
+        
+    },
+    
+})
+
+export default Card;
